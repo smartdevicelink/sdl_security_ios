f7bd994d4850f95ee300f90d1529c7bb7df6c357
NicoleYarroch <nicole@livio.io>
2019-09-19 17:26:20 -0400

    Making build work with simulator

---
+++ Dependencies/OpenSSL/MissingOpenSSLSimulatorFuncs.m
@@ -0,0 +1,9 @@
+//
+//  MissingOpenSSLSimulatorFuncs.m
+//  SDLSecurity
+//
+//  Created by Nicole on 9/19/19.
+//  Copyright © 2019 livio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
