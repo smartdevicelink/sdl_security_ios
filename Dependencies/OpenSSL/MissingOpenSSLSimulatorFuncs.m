440d51458f880ccfab8f2bb73562e2425bac3631
NicoleYarroch <nicole@livio.io>
2019-09-19 17:26:51 -0400

    Making build work with Xcode simulator

--- Dependencies/OpenSSL/MissingOpenSSLSimulatorFuncs.m
+++ Dependencies/OpenSSL/MissingOpenSSLSimulatorFuncs.m
@@ -1,9 +1,19 @@
-//
-//  MissingOpenSSLSimulatorFuncs.m
-//  SDLSecurity
-//
-//  Created by Nicole on 9/19/19.
-//  Copyright © 2019 livio. All rights reserved.
-//
+// https://stackoverflow.com/questions/29390112/libcrypto-a-symbols-not-found-for-architecture-i386
 
-#import <Foundation/Foundation.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <fnmatch.h>
+
+DIR * opendir$INODE64( char * dirName )
+{
+    return opendir( dirName );
+}
+
+struct dirent * readdir$INODE64( DIR * dir )
+{
+    return readdir( dir );
+}
+
