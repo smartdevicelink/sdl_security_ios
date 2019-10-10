7fc8084c2219d68f97698a06683a9110deb21b28
NicoleYarroch <nicole@livio.io>
2019-10-10 10:11:30 -0400

    Refactored certficate download code

--- SDLSecurity/_SDLCertificateManager.m
+++ SDLSecurity/_SDLCertificateManager.m
@@ -84,71 +84,52 @@
 - (void)retrieveNewCertificateWithAppId:(NSString *)appId completionHandler:(SDLCertificateRetrievedHandler)completionHandler {
     SDLSecurityLogD(@"Performing network request for certificate");
 
-    // Create the URL request with the required parameters
-    NSMutableURLRequest *request = [[NSURLRequest requestWithURL:self.certificateURL] mutableCopy];
-    if (request == nil) {
-        SDLSecurityLogE(@"Error creating security request from URL");
-        completionHandler(NO, nil); // TODO: Error
-        return;
-    }
-    
-    [request setValue:@"application/json" forHTTPHeaderField:@"content-type"];
-    [request setValue:@"application/json" forHTTPHeaderField:@"accept"];
-    
-    // Attempt to serialize the app id dictionary into json data
-    NSError *jsonEncodeError = nil;
-    NSDictionary *appIdJSON = @{@"appId": appId};
-    NSData *appIdData = [NSJSONSerialization dataWithJSONObject:appIdJSON options:0 error:&jsonEncodeError];
-    if (appIdData == nil) {
-        SDLSecurityLogE(@"Error creating security request JSON");
-        completionHandler(NO, jsonEncodeError);
-        return;
-    }
+    NSString *certificateURL = [NSString stringWithFormat:@"%@?appID=%@", self.certificateURL.absoluteString, appId];
 
-    [[NSURLSession sharedSession] uploadTaskWithRequest:request fromData:appIdData completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-        // If the certificate server didn't send us any data back
-        if (data == nil) {
-            SDLSecurityLogE(@"Security server responded with an error. Response: %@, Error: %@", response, error);
-            completionHandler(NO, error);
-            return;
+    SDLSecurityLogD(@"Getting certificate data from: %@", certificateURL);
+
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURL *url = [NSURL URLWithString:certificateURL];
+    NSURLSessionDataTask *task = [session dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+
+        if (data.length == 0) {
+            SDLSecurityLogE(@"No certificate data returned");
+            // TODO: return custom error
+            return completionHandler(NO, nil);
         }
 
-        // Try to decode the server data
-        NSError *jsonDecodeError = nil;
-        NSDictionary *jsonDecodedDict = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonDecodeError];
-        if ((jsonDecodedDict == nil) || (jsonDecodedDict[@"Certificate"] == nil)) {
-            SDLSecurityLogE(@"Error decoding JSON: %@", jsonDecodeError);
-            completionHandler(NO, jsonDecodeError);
-            return;
+        NSError *jsonError = nil;
+        NSArray *jsonArray = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+        NSDictionary *jsonDictionary = jsonArray.firstObject;
+
+        if (jsonError != nil || jsonDictionary == nil) {
+            SDLSecurityLogE(@"Error parsing JSON");
+            // TODO: return custom error
+            return completionHandler(NO, nil);
         }
 
-        // The cert is base 64 encoded. Decode it
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
-        NSData *base64Decoded = [[NSData alloc] initWithBase64Encoding:jsonDecodedDict[@"Certificate"]];
-#pragma clang diagnostic pop
+        NSData *base64Decoded = [[NSData alloc] initWithBase64EncodedData:[jsonDictionary objectForKey:@"certificate"] options:0];
+
         if (base64Decoded == nil) {
-            SDLSecurityLogE(@"TLS security certificate is invalid");
-            completionHandler(NO, [NSError errorWithDomain:SDLSecurityErrorDomain code:SDLTLSErrorCodeCertificateInvalid userInfo:@{NSLocalizedDescriptionKey: @"TLS security certificate is invalid"}]);
+            SDLSecurityLogE(@"Security certificate is invalid");
+            return completionHandler(NO, [NSError errorWithDomain:SDLSecurityErrorDomain code:SDLTLSErrorCodeCertificateInvalid userInfo:nil]);
         }
 
-        // TODO: Encrypt?
-
-        // TODO: Weakself
         // We have the cert data, store it as a file in the correct path
         NSError *writeFileError = nil;
-        [[self class] sdl_deleteCertificate];
+        [self.class sdl_deleteCertificate];
         BOOL writeSuccess = [base64Decoded writeToFile:[[self class] sdl_certificateFilePath] options:0 error:&writeFileError];
         if (!writeSuccess) {
             SDLSecurityLogE(@"Error writing TLS certificate to disk: %@", writeFileError);
-            completionHandler(NO, writeFileError);
-            return;
+            return completionHandler(NO, writeFileError);
         }
 
         // Everything succeeded, the new cert exists at the file path
         SDLSecurityLogD(@"TLS certificate downloaded successfully");
-        completionHandler(YES, nil);
+        return completionHandler(YES, nil);
     }];
+
+    [task resume];
 }
 
 @end
