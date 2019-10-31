f384bdd21a305c30877c43d353300ce646c25b6c
NicoleYarroch <nicole@livio.io>
2019-10-31 13:27:02 -0400

    Added documentation to SDLPrivateSecurityConstants

--- SDLSecurity/SDLPrivateSecurityConstants.h
+++ SDLSecurity/SDLPrivateSecurityConstants.h
@@ -8,15 +8,24 @@
 
 #import <Foundation/Foundation.h>
 
+/// The URL from which to download the PFX certificate file data
 extern NSString *const CertDevURL;
 extern NSString *const CertQAURL;
 extern NSString *const CertProdURL;
-extern NSString *const VendorName;
+
+/// The issuer name of the PFX certificate file
 extern NSString *const SDLTLSIssuer;
+
+/// The password used to generate the PFX certificate file
 extern const char *SDLTLSCertPassword;
 
+/// The name of the custom directory folder where the downloaded certificate will be stored
+extern NSString *const VendorName;
+
+/// Private constants used by the security manager.
 @interface SDLPrivateSecurityConstants : NSObject
 
+/// The vehicle types this security library supports.
 + (NSSet<NSString *> *)availableMakes;
 
 @end
