f7f9ebfa8d6dbf117199f9273d8b562577dec189
NicoleYarroch <nicole@livio.io>
2019-11-06 09:12:53 -0500

    Added description to error enum

--- SDLSecurity/SDLSecurityConstants.h
+++ SDLSecurity/SDLSecurityConstants.h
@@ -8,19 +8,32 @@
 
 #import <Foundation/Foundation.h>
 
+/// Custom error domain for the security library
 extern NSString *const SDLSecurityErrorDomain;
 
+/// An enum for all the errors returned by the security library
 typedef NS_ENUM(NSUInteger, SDLTLSErrorCode) {
+    /// No error occured
     SDLTLSErrorCodeNone,
+    /// A non-recoverable, fatal error in the SSL library occurred
     SDLTLSErrorCodeSSL,
+    /// The OpenSSL read operation did not complete
     SDLTLSErrorCodeWantRead,
+    /// The OpenSSL write operation did not complete
     SDLTLSErrorCodeWantWrite,
+    /// The OpenSSL write operation failed
     SDLTLSErrorCodeWriteFailed,
+    /// An unknown error occured with the OpenSSL library
     SDLTLSErrorCodeGeneric,
+    /// The DTLS/SSL connection is not in state where fully protected data can be transferred
     SDLTLSErrorCodeNotInitialized,
+    /// The PFX file's keys or certificate parameters are incorrect
     SDLTLSErrorCodeInitializationFailure,
+    /// There is no certificate
     SDLTLSErrorCodeNoCertificate,
+    /// The certificate is expired
     SDLTLSErrorCodeCertificateExpired,
+    /// The certificate data is missing or invalid
     SDLTLSErrorCodeCertificateInvalid
 };
 
