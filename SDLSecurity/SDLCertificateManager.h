a5868adf77b5ddddb7a5d62f8cf381dd57f9e379
NicoleYarroch <nicole@livio.io>
2019-11-14 14:20:44 -0500

    Added documentation to all classes (#43)
    * Added docs to SDLSecurityManager
    
    * Added documentation to SDLSecurityManager
    
    * Added documentation to SDLPrivateSecurityConstants
    
    * Added missing NONNULL macros
    
    * Added documentation to logger classes
    
    * Added documentation to SDLSecurityType protocol
    
    * Added documentation to the TLSEngine
    
    * Added initial docs to SDLTLSEngine class
    
    * Added description to error enum
    
    * Added docs to the SDLTLSEngine class
    
    * Fixed enum docs
    
    * Added docs to the SDLCertificateManager class
    
    * Spelling and grammar cleanup
    
    * Apply suggestions from code review
    
    Co-Authored-By: justingluck93 <47197545+justingluck93@users.noreply.github.com>

--- SDLSecurity/SDLCertificateManager.h
+++ SDLSecurity/SDLCertificateManager.h
@@ -10,15 +10,26 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/// A completion handler called when the certificate retrieval has finished.
+/// @param success True if the certficate was downloaded and cached successfully; false if an error occured
+/// @param error The error that occured during the certificate downloaded, if any occurred
 typedef void (^SDLCertificateRetrievedHandler)(BOOL success, NSError *__nullable error);
 
-
+/// Manager class for downloading and caching a PFX file (A PFX file contains the certificate, public key, and private key).
 @interface SDLCertificateManager : NSObject
 
+/// The cached PFX file data.
 @property (nonatomic, copy, readonly, nullable) NSData *certificateData;
 
 - (instancetype)init NS_UNAVAILABLE;
+
+/// Initializes the certificate manager with the URL where all the PFX files are hosted.
+/// @param url The URL where the PFX files are being hosted.
 - (instancetype)initWithCertificateServerURL:(NSString *)url;
+
+/// Retrieves the unique PFX file associated with the appId. To retrieve the PFX file, the passed appId is added to the url as a query string for the key `appId`. The downloaded PFX data is then cached on disk.
+/// @param appId The appID of the SDL app
+/// @param completionHandler Handler called when certificate retrieval has finished
 - (void)retrieveNewCertificateWithAppId:(NSString *)appId completionHandler:(SDLCertificateRetrievedHandler)completionHandler;
 
 @end
--- SDLSecurity/SDLSecurityLoggerMacros.h
+++ SDLSecurity/SDLSecurityLoggerMacros.h
@@ -11,10 +11,19 @@
 #ifndef SDLSecurityLoggerMacros_h
 #define SDLSecurityLoggerMacros_h
 
+/// Log a debug log
+/// @param msg The format string to log
+/// @param ... The format arguments to log
 #define SDLSecurityLogD(msg, ...) [[SDLSecurityLogger shared] logWithLevel:LoggerLevelDebug timestamp:[NSDate date] message:msg, ##__VA_ARGS__]
 
+/// Log a warning log
+/// @param msg The format string to log
+/// @param ... The format arguments to log
 #define SDLSecurityLogW(msg, ...) [[SDLSecurityLogger shared] logWithLevel:LoggerLevelWarning timestamp:[NSDate date] message:msg, ##__VA_ARGS__]
 
+/// Log an error log
+/// @param msg The format string to log
+/// @param ... The format arguments to log
 #define SDLSecurityLogE(msg, ...) [[SDLSecurityLogger shared] logWithLevel:LoggerLevelError timestamp:[NSDate date] message:msg, ##__VA_ARGS__]
 
 #endif /* SDLSecurityLoggerMacros_h */
